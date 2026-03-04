4b48af380d9a1abbc25c1c1db8843a8c8173b01b
rfojta <rfojta@gmail.com>
2026-03-04 20:51:54 +0100

    removed separated and tetris-split

--- separated/tetris_game/DIContainer.js
+++
@@ -1,5 +0,0 @@
-class DIContainer {
-    constructor() { this.services = new Map(); }
-    register(name, instance) { this.services.set(name, instance); }
-    get(name) { return this.services.get(name); }
-}
--- tetris_split/js/pieces.js
+++
@@ -1,9 +0,0 @@
-export const pieces = [
-  { shape: ['xxxx'], color: 0 },
-  { shape: [' x ', 'xxx'], color: 1 },
-  { shape: [' xx', 'xx '], color: 2 },
-  { shape: ['xx', 'xx'], color: 3 },
-  { shape: ['  x', 'xxx'], color: 4 },
-  { shape: ['x  ', 'xxx'], color: 5 },
-  { shape: ['xx ', ' xx'], color: 6 },
-];
